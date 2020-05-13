ce1e82ffc0ff90eb0eb60f75b68fd0f38e92c761
Ian McElroy <mcelroyian@gmail.com>
2020-05-13 15:53:56 -0500

    scaffolding provider and store

---
+++ brewski/src/store/reducers/index.js
@@ -0,0 +1,12 @@
+export const initialState = {
+
+}
+
+export const beerReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return {
+                ...state
+            }
+    }
+}
